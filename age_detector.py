626b14b10f214d90e77cba5ca404d166a39e36bc
Dmitry Ovchinnikov <ovchinnikov.d94@gmail.com>
2015-11-18 15:47:29 +0300

    first commit

---
+++ age_detector.py
@@ -0,0 +1,24 @@
+__author__ = 'Dmitriy Ovchinnikov'
+
+
+class AgeDetector:
+
+    def __init__(self):
+        """
+        it is constructor
+        """
+        pass
+    def train(self, instances, labels):
+        """
+        Train method
+        """
+        pass
+        
+
+    def classify(self, instances):
+        """
+        Classify method 
+        """
+        pass
+if __name__ == '__main__':
+    return
